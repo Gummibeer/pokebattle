f95df7f8f49e0bd816966cb0a1d63de4f5fa8737
Tom Witkowski <dev.gummibeer@gmail.com>
2015-11-04 22:35:20 +0100

    switch tab order in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,23 +2,14 @@
     <div class="sb-content">
         <div class="tab-navigation">
             <ul class="nav nav-tabs nav-justified">
-                <li class="active"><a href="#sidebar-history" data-toggle="tab" class="text-center"><i class="icon wh-history"></i></a></li>
-                <li><a href="#sidebar-battles" data-toggle="tab" class="text-center"><i class="icon wh-trophy"></i></a></li>
+                <li class="active"><a href="#sidebar-battles" data-toggle="tab" class="text-center"><i class="icon wh-trophy"></i></a></li>
+                <li><a href="#sidebar-history" data-toggle="tab" class="text-center"><i class="icon wh-history"></i></a></li>
             </ul>
         </div>
 
         <div class="tab-panel">
             <div class="tab-content white">
-                <div class="am-scroller nano tab-pane active" id="sidebar-history">
-                    <div class="nano-content">
-                        <div class="content padding-15">
-                            @foreach($battlemessages as $battlemessage)
-                                {!! \Michelf\MarkdownExtra::defaultTransform($battlemessage->message) !!}
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="am-scroller nano tab-pane" id="sidebar-battles">
+                <div class="am-scroller nano tab-pane active" id="sidebar-battles">
                     <div class="nano-content">
                         <div class="content padding-15">
                             @foreach($battlehistories as $battlehistory)
@@ -27,6 +18,15 @@
                                     vs
                                     <strong class="@if(!$battlehistory->attacker_win) text-primary @endif" data-toggle="tooltip" data-placement="bottom" data-title="{{ $battlehistory->defenderPokemon->display_name }}">{{ $battlehistory->defender->name }}</strong>
                                 </p>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="am-scroller nano tab-pane" id="sidebar-history">
+                    <div class="nano-content">
+                        <div class="content padding-15">
+                            @foreach($battlemessages as $battlemessage)
+                                {!! \Michelf\MarkdownExtra::defaultTransform($battlemessage->message) !!}
                             @endforeach
                         </div>
                     </div>
