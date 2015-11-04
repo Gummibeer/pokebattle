87bd4766e73de66473f84563abfdacc04b7f69ee
Tom Witkowski <dev.gummibeer@gmail.com>
2015-11-04 21:13:50 +0100

    disable oAuth - too much problems

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -11,27 +11,6 @@
 
             @yield('content')
 
-            <div class="form-group row">
-                <div class="col-sm-6">
-                    <a href="{{ url('auth/facebook') }}" class="btn btn-block btn-labeled social-facebook">
-                        <span class="btn-label"><i class="icon wh-facebook"></i></span>
-                        Facebook
-                    </a>
-                </div>
-                <div class="col-sm-6">
-                    <a href="{{ url('auth/github') }}" class="btn btn-block btn-labeled social-github">
-                        <span class="btn-label"><i class="icon wh-github"></i></span>
-                        Github
-                    </a>
-                </div>
-                <div class="col-sm-6">
-                    <a href="{{ url('auth/slack') }}" class="btn btn-block btn-labeled social-slack">
-                        <span class="btn-label"><i class="icon wh-chat"></i></span>
-                        Slack
-                    </a>
-                </div>
-            </div>
-
             <div class="text-center out-links">© 2015 <a href="https://gummibeer.de">Gummibeer</a></div>
         </div>
     </div>
